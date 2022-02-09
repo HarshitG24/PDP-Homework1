40acbda1de9fb46a96331096d92a1c76606f1ff8
Harshit Gajjar <hrstgajjar3@gmail.com>
2022-02-09 07:59:37 -0800

    added publicationManager and fixes for publication and website class

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 class Publication {
-  constructor(title, year, author, publisher, location, year) {
+  constructor(title, year) {
     // check if both the values of title and year are valid
     if (!title || !year) {
       throw new Error("Need to provide title and year correctly");
@@ -76,26 +76,68 @@ class Paper extends Publication {
 }
 
 class WebPage extends Publication {
-  constructor(title, year, url, date) {
-    if (!url || !date) {
+  constructor(title, year, url) {
+    if (!url) {
       throw new Error("Invalid URL");
     }
     super(title, year);
     this.url = url;
-    this.date = date;
   }
 
   isWebPage() {
-    return (
-      this.isPublication() && this.url !== undefined && this.date !== undefined
-    );
+    return this.isPublication() && this.url !== undefined;
   }
 
+  //   formatDate(date = new Date()) {
+  //     let dt = new Date(date);
+  //     let dateString =
+  //       `${dt.getMonth() + 1}` + "/" + dt.getDate() + "/" + dt.getFullYear();
+  //     console.log(dateString);
+  //   }
+
   citeAPA() {
-    return `${this.title}. Retrieved ${this.date}, from ${this.url}.`;
+    return `${this.title}. Retrieved ${this.year}, from ${this.url}.`;
   }
 
   citeMLA() {
-    return `\${this.title}. Web. ${this.date} <this.url>.`;
+    return `${this.title}. Web. ${this.url} ${this.year}.`;
   }
 }
+
+class PublicationManager {
+  constructor() {
+    this.publications = [];
+  }
+
+  addPaper(title, author, year, journal, volume) {
+    this.publications.push(new Paper(title, author, year, journal, volume));
+  }
+
+  addBook(title, author, year, publisher) {
+    this.publications.push(new Book(title, author, year, publisher));
+  }
+
+  addWebsite(title, url, year) {
+    this.publications.push(new WebPage(title, url, year));
+  }
+
+  printCitations(type) {
+    for (let pub of this.publications) {
+      if (type === "APA") console.log(pub.citeAPA());
+      else console.log(pub.citeMLA());
+    }
+  }
+}
+
+const manager = new PublicationManager();
+manager.addBook("a", "b", "c", "d", "e", "f");
+
+manager.addBook("Rationality", "Christopher", 2021, "Oveja Negra");
+
+manager.addWebsite(
+  "Programming Paradigms",
+  "https://course.ccs.neu.edu/cs5010f18/lecture1.html",
+  2005,
+  new Date()
+);
+manager.printCitations("APA");
